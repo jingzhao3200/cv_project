810b653bcf5d41e19308be2678451cde9628272f
jingzhao <ms.zj@outlook.com>
2019-04-28 23:10:04 -0400

    run version

--- AutoDeeplab/train_voc.sh
+++ AutoDeeplab/train_voc.sh
@@ -1,3 +1,3 @@
-CUDA_VISIBLE_DEVICES=0 python train_autodeeplab.py --backbone xception --lr 0.01 \
+CUDA_VISIBLE_DEVICES=0 python train_autodeeplab.py --backbone xception --lr 0.01 --arch-lr 0.01 \
 --workers 4 --epochs 100 --batch-size 2 --gpu-ids 0 \
 --checkname auto-deeplab-kitti --eval-interval 1 --dataset kitti
